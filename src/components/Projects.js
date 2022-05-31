990d44c87af57a1807f8f46132be065e4fa24bc3
Heriberto Lopez Jr <hl_9023@yahoo.com>
2022-05-31 17:25:22 -0500

    5/31 mods to projectB

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,17 @@
 
+import '../styles/Projects.css'
+
 
 function Projects() {
     return (
-        <div>
+        <div className="projectsDiv">
+            <div className="projectB" >
             <img src="https://www.tshaonline.org/images/handbook/entries/BB/brown-county-2016.jpg" alt="54544"></img>
+            <text>
+                pkfwrjnfkndlsk djfndks fdjkfnsdkmd klcsd cjsdlnfkldmckldsc dc lkdsckx;cjsmcdskcd cd ckldmcksdmcvkldsmcv dc lkmnc klmc klsdmclnsd c zmdc.x cjldmvksmldm . cjdsncskdmcksdnjfndsjnkdsmc  dkcnd fdnfd m smd cdkvm v. jdncklsdnck sdkmc ksd cmxc sx cjdsckdkmcksdm. jsndjncd cni
+            </text>
+          
+            </div>
         </div>
     )
 }
